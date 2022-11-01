8647d8c9a84a922ea87934b2e989079a67a6f730
Andrew Martin <imchet.w@gmail.com>
2022-11-01 19:59:30 -0400

    HuskyBot in progress

--- functions.py
+++ functions.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 
 # Function Definitions
-
 def getCurrentDateTime():
     return f'{datetime.now().strftime("%m/%d/%Y")} at {(datetime.strptime(datetime.now().strftime("%H:%M:%S"), "%H:%M:%S")).strftime("%r")}'
 
